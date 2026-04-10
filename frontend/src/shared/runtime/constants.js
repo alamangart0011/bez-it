232659cc51406752ef648cbdc7b02055551fa000
alamanqart001 <alamangart001@gmail.com>
2026-04-10 08:35:04 +0300

    frontend: extract runtime constants

---
+++ frontend/src/shared/runtime/constants.js
@@ -0,0 +1,8 @@
+export const STORAGE_KEYS = {
+  token: 'sg_token',
+};
+
+export const WS_RECONNECT_DELAY_MS = 3000;
+export const VOICE_POLL_INTERVAL_MS = 8000;
+export const TOAST_DURATION_MS = 3500;
+export const MESSAGE_GROUP_WINDOW_MS = 300000;
