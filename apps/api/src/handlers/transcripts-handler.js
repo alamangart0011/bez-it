65cf2ed6fa18ad9bf78e212e1c88223ef1d5141b
alamanqart001 <alamangart001@gmail.com>
2026-04-10 23:25:06 +0300

    feat: add transcripts handler skeleton

---
+++ apps/api/src/handlers/transcripts-handler.js
@@ -0,0 +1,9 @@
+module.exports = {
+  domain: 'transcripts',
+  handlers: {
+    startTranscript: 'startTranscript',
+    appendChunk: 'appendChunk',
+    listTranscript: 'listTranscript',
+    buildSummary: 'buildSummary'
+  }
+};
