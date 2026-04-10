e5be1cc9b5c6b6f7d1e5c97501e8aab4605f6c9a
alamanqart001 <alamangart001@gmail.com>
2026-04-10 22:11:39 +0300

    feat: add web api bindings index

---
+++ apps/web/src/api/bindings/index.ts
@@ -0,0 +1,11 @@
+import { roomsBinding } from './rooms';
+import { callsBinding } from './calls';
+import { profileBinding } from './profile';
+import { adminBinding } from './admin';
+
+export const webApiBindings = {
+  rooms: roomsBinding,
+  calls: callsBinding,
+  profile: profileBinding,
+  admin: adminBinding
+};
