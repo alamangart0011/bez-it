bb98f52b62f5bf8196155f78abdd570cdda4b054
alamanqart001 <alamangart001@gmail.com>
2026-04-10 06:38:57 +0300

    brand: add Signum brand meta

---
+++ frontend/src/brand/meta.js
@@ -0,0 +1,9 @@
+export const brandMeta = {
+  appName: 'Сигнум',
+  shortName: 'Сигнум',
+  subtitle: 'корпоративная связь и комнаты',
+  themeColor: '#19C37D',
+  accentColor: '#19C37D',
+  accentColorStrong: '#119C67',
+  accentColorSoft: '#3BE38F',
+};
