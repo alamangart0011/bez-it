43aca52fa9a0ba053c4fe1f3480b16ec3bb2695d
alamanqart001 <alamangart001@gmail.com>
2026-04-10 22:31:25 +0300

    feat: add rooms handler skeleton

---
+++ apps/api/src/handlers/rooms-handler.js
@@ -0,0 +1,10 @@
+module.exports = {
+  domain: 'rooms',
+  handlers: {
+    listRooms: 'listRooms',
+    getRoom: 'getRoom',
+    createRoom: 'createRoom',
+    joinRoom: 'joinRoom',
+    leaveRoom: 'leaveRoom'
+  }
+};
