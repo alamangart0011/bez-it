4cfd7a7f188de0df1e1485adf628097808b026f7
alamanqart001 <alamangart001@gmail.com>
2026-04-10 22:54:15 +0300

    feat: add topbar shell model

---
+++ apps/web/src/shell/topbar-shell.ts
@@ -0,0 +1,5 @@
+export const topbarShell = {
+  sections: ['title', 'presence', 'controls', 'search'],
+  actions: ['openSearch', 'openProfile', 'togglePresence'],
+  states: ['default', 'callActive']
+};
