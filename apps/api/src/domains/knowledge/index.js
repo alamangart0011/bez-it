84effd428776bc07a7a809497107f39ae990539e
alamanqart001 <alamangart001@gmail.com>
2026-04-10 21:04:20 +0300

    feat: add knowledge runtime skeleton

---
+++ apps/api/src/domains/knowledge/index.js
@@ -0,0 +1,10 @@
+module.exports = {
+  domain: 'knowledge',
+  operations: [
+    'registerSource',
+    'indexDocument',
+    'searchKnowledge',
+    'getContextForRoom',
+    'getContextForUser'
+  ]
+};
