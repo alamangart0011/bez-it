73f528689bc505c72064824f487dfae2aefa37e5
alamanqart001 <alamangart001@gmail.com>
2026-04-10 23:27:52 +0300

    feat: add calls runtime view model

---
+++ apps/web/src/views/calls-view.ts
@@ -0,0 +1,7 @@
+export const callsView = {
+  shell: 'callShell',
+  adapter: 'callsPageAdapter',
+  loader: 'callsLoader',
+  sections: ['callHeader', 'participantGrid', 'controls', 'transcriptPanel'],
+  actions: ['joinCall', 'leaveCall', 'toggleMute', 'openAssistant']
+};
